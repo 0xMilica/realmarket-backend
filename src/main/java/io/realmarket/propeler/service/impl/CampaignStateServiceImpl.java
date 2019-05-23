d057bf9a57295720157c241179fea30d8190edb2
vulicmilica <milica.vulic@realmarket.io>
2019-05-23 16:23:09 +0200

    [PROP-269] Sonar Lint issues resolved, smaller improvments

--- src/main/java/io/realmarket/propeler/service/impl/CampaignStateServiceImpl.java
+++ src/main/java/io/realmarket/propeler/service/impl/CampaignStateServiceImpl.java
@@ -20,7 +20,8 @@ public class CampaignStateServiceImpl implements CampaignStateService {
     this.campaignService = campaignService;
   }
 
-  private Map<CampaignStateName, List<CampaignStateName>> stateChangeFlow = createAndInitStateChangeFlow();
+  private Map<CampaignStateName, List<CampaignStateName>> stateTransitFlow =
+      createAndInitStateChangeFlow();
   private Map<CampaignState, List<EUserRole>> rolesPerState = createAndInitRolesPerState();
 
   @Override
@@ -29,7 +30,10 @@ public class CampaignStateServiceImpl implements CampaignStateService {
     CampaignState currentCampaignState = campaign.getCampaignState();
     EUserRole eUserRole = AuthenticationUtil.getAuthentication().getAuth().getUserRole();
 
-    if (stateChangeFlow.get(currentCampaignState).contains(followingCampaignState.getName())
+    if (stateTransitFlow
+            .get(CampaignStateName.fromString(currentCampaignState.getName()))
+            .toString()
+            .equals(followingCampaignState.getName())
         && rolesPerState.get(currentCampaignState).contains(eUserRole)) {
       campaign.setCampaignState(followingCampaignState);
     }
@@ -46,33 +50,44 @@ public class CampaignStateServiceImpl implements CampaignStateService {
   }
 
   private Map createAndInitStateChangeFlow() {
-    Map<CampaignStateName, List<CampaignStateName>> stateChangeFlow = new EnumMap<>(CampaignStateName.class);
-    stateChangeFlow.put(CampaignStateName.INITIAL, Arrays.asList(CampaignStateName.REVIEW_READY));
-    stateChangeFlow.put(CampaignStateName.REVIEW_READY, Arrays.asList(CampaignStateName.AUDIT));
-    stateChangeFlow.put(
+    Map<CampaignStateName, List<CampaignStateName>> campaignStateTransitFlow =
+        new EnumMap<>(CampaignStateName.class);
+    campaignStateTransitFlow.put(
+        CampaignStateName.INITIAL, Collections.singletonList(CampaignStateName.REVIEW_READY));
+    campaignStateTransitFlow.put(
+        CampaignStateName.REVIEW_READY, Collections.singletonList(CampaignStateName.AUDIT));
+    campaignStateTransitFlow.put(
         CampaignStateName.AUDIT,
         Arrays.asList(CampaignStateName.INITIAL, CampaignStateName.FINANCE_PROPOSITION));
-    stateChangeFlow.put(
+    campaignStateTransitFlow.put(
         CampaignStateName.FINANCE_PROPOSITION,
         Arrays.asList(CampaignStateName.INITIAL, CampaignStateName.LEAD_INVESTMENT));
-    stateChangeFlow.put(
+    campaignStateTransitFlow.put(
         CampaignStateName.LEAD_INVESTMENT,
         Arrays.asList(CampaignStateName.INITIAL, CampaignStateName.ACTIVE));
-    stateChangeFlow.put(CampaignStateName.ACTIVE, Arrays.asList(CampaignStateName.POST_CAMPAIGN));
-    stateChangeFlow.put(CampaignStateName.POST_CAMPAIGN, Arrays.asList());
-    return stateChangeFlow;
+    campaignStateTransitFlow.put(
+        CampaignStateName.ACTIVE, Collections.singletonList(CampaignStateName.POST_CAMPAIGN));
+    campaignStateTransitFlow.put(CampaignStateName.POST_CAMPAIGN, Collections.emptyList());
+    return campaignStateTransitFlow;
   }
 
   private Map createAndInitRolesPerState() {
-    Map<CampaignStateName, List<EUserRole>> rolesPerState = new EnumMap<>(CampaignStateName.class);
-    rolesPerState.put(CampaignStateName.INITIAL, Arrays.asList(EUserRole.ROLE_ENTREPRENEUR));
-    rolesPerState.put(CampaignStateName.REVIEW_READY, Arrays.asList(EUserRole.ROLE_AUDITOR));
-    rolesPerState.put(CampaignStateName.AUDIT, Arrays.asList(EUserRole.ROLE_AUDITOR));
-    rolesPerState.put(
-        CampaignStateName.FINANCE_PROPOSITION, Arrays.asList(EUserRole.ROLE_ENTREPRENEUR));
-    rolesPerState.put(CampaignStateName.LEAD_INVESTMENT, Arrays.asList(EUserRole.ROLE_ADMIN));
-    rolesPerState.put(CampaignStateName.ACTIVE, Arrays.asList(EUserRole.ROLE_ADMIN));
-    rolesPerState.put(CampaignStateName.POST_CAMPAIGN, Arrays.asList());
-    return rolesPerState;
+    Map<CampaignStateName, List<EUserRole>> rolesPerCampaignState =
+        new EnumMap<>(CampaignStateName.class);
+    rolesPerCampaignState.put(
+        CampaignStateName.INITIAL, Collections.singletonList(EUserRole.ROLE_ENTREPRENEUR));
+    rolesPerCampaignState.put(
+        CampaignStateName.REVIEW_READY, Collections.singletonList(EUserRole.ROLE_AUDITOR));
+    rolesPerCampaignState.put(
+        CampaignStateName.AUDIT, Collections.singletonList(EUserRole.ROLE_AUDITOR));
+    rolesPerCampaignState.put(
+        CampaignStateName.FINANCE_PROPOSITION,
+        Collections.singletonList(EUserRole.ROLE_ENTREPRENEUR));
+    rolesPerCampaignState.put(
+        CampaignStateName.LEAD_INVESTMENT, Collections.singletonList(EUserRole.ROLE_ADMIN));
+    rolesPerCampaignState.put(
+        CampaignStateName.ACTIVE, Collections.singletonList(EUserRole.ROLE_ADMIN));
+    rolesPerCampaignState.put(CampaignStateName.POST_CAMPAIGN, Collections.emptyList());
+    return rolesPerCampaignState;
   }
 }
