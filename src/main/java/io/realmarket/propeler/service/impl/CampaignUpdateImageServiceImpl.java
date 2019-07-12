1c174f62c848d501ae06bbd3bd6c4b6fe17d5105
Angelina Vujanović <angelina.vujanovic@realmarket.io>
2019-07-12 07:06:32 +0000

    Merged in bugfix/PROP-764-delete-campaign-update (pull request #191)
    [PROP-764] Bug fixed
    
    * [PROP-764] Bug fixed
    
    * [PROP-764] Fix
    
    Approved-by: SA Bitbucket <sa.bitbucket@realmarket.io>
    Approved-by: Ognjen Vlajić <ognjen.vlajic@realmarket.io>

--- src/main/java/io/realmarket/propeler/service/impl/CampaignUpdateImageServiceImpl.java
+++ src/main/java/io/realmarket/propeler/service/impl/CampaignUpdateImageServiceImpl.java
@@ -10,65 +10,72 @@ import io.realmarket.propeler.service.CampaignUpdateService;
 import io.realmarket.propeler.service.CloudObjectStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class CampaignUpdateImageServiceImpl implements CampaignUpdateImageService {
 
-  private final CampaignUpdateImageRepository campaignUpdateImageRepository;
-  private final CampaignService campaignService;
-  private final CampaignUpdateService campaignUpdateService;
-  private final CloudObjectStorageService cloudObjectStorageService;
+    private final CampaignUpdateImageRepository campaignUpdateImageRepository;
+    private final CampaignService campaignService;
+    private final CampaignUpdateService campaignUpdateService;
+    private final CloudObjectStorageService cloudObjectStorageService;
 
-  @Autowired
-  public CampaignUpdateImageServiceImpl(
-      CampaignUpdateImageRepository campaignUpdateImageRepository,
-      CampaignService campaignService,
-      @Lazy CampaignUpdateService campaignUpdateService,
-      CloudObjectStorageService cloudObjectStorageService) {
-    this.campaignUpdateImageRepository = campaignUpdateImageRepository;
-    this.campaignService = campaignService;
-    this.campaignUpdateService = campaignUpdateService;
-    this.cloudObjectStorageService = cloudObjectStorageService;
-  }
+    @Autowired
+    public CampaignUpdateImageServiceImpl(
+            CampaignUpdateImageRepository campaignUpdateImageRepository,
+            CampaignService campaignService,
+            @Lazy CampaignUpdateService campaignUpdateService,
+            CloudObjectStorageService cloudObjectStorageService) {
+        this.campaignUpdateImageRepository = campaignUpdateImageRepository;
+        this.campaignService = campaignService;
+        this.campaignUpdateService = campaignUpdateService;
+        this.cloudObjectStorageService = cloudObjectStorageService;
+    }
 
-  @Override
-  public void removeObsoleteImages(CampaignUpdate campaignUpdate) {
-    campaignUpdateImageRepository
-        .findByCampaignUpdate(campaignUpdate)
-        .forEach(
-            campaignUpdateImage -> {
-              if (!campaignUpdate.getContent().contains(campaignUpdateImage.getUrl())) {
-                cloudObjectStorageService.delete(campaignUpdateImage.getUrl());
-                campaignUpdateImageRepository.delete(campaignUpdateImage);
-              }
-            });
-  }
+    @Override
+    @Async
+    @Transactional
+    public void removeObsoleteImages(CampaignUpdate campaignUpdate) {
+        campaignUpdateImageRepository
+                .findByCampaignUpdate(campaignUpdate)
+                .forEach(
+                        campaignUpdateImage -> {
+                            if (!campaignUpdate.getContent().contains(campaignUpdateImage.getUrl())) {
+                                cloudObjectStorageService.delete(campaignUpdateImage.getUrl());
+                                campaignUpdateImageRepository.delete(campaignUpdateImage);
+                            }
+                        });
+    }
 
-  @Override
-  public FilenameDto uploadImage(Long campaignUpdateId, MultipartFile image) {
-    CampaignUpdate campaignUpdate =
-        campaignUpdateService.findByIdOrThrowException(campaignUpdateId);
+    @Override
+    @Transactional
+    public FilenameDto uploadImage(Long campaignUpdateId, MultipartFile image) {
+        CampaignUpdate campaignUpdate =
+                campaignUpdateService.findByIdOrThrowException(campaignUpdateId);
 
-    campaignService.throwIfNotOwner(campaignUpdate.getCampaign());
-    campaignService.throwIfNotActive(campaignUpdate.getCampaign());
+        campaignService.throwIfNotOwner(campaignUpdate.getCampaign());
+        campaignService.throwIfNotActive(campaignUpdate.getCampaign());
 
-    String url = cloudObjectStorageService.uploadPublic(image);
-    campaignUpdateImageRepository.save(
-        CampaignUpdateImage.builder().url(url).campaignUpdate(campaignUpdate).build());
+        String url = cloudObjectStorageService.uploadPublic(image);
+        campaignUpdateImageRepository.save(
+                CampaignUpdateImage.builder().url(url).campaignUpdate(campaignUpdate).build());
 
-    return new FilenameDto(String.join("", FileServiceImpl.PUBLIC_FILE_ENDPOINT, url));
-  }
+        return new FilenameDto(String.join("", FileServiceImpl.PUBLIC_FILE_ENDPOINT, url));
+    }
 
-  @Override
-  public void removeImages(CampaignUpdate campaignUpdate) {
-    campaignUpdateImageRepository
-        .findByCampaignUpdate(campaignUpdate)
-        .forEach(
-            campaignUpdateImage -> {
-              cloudObjectStorageService.delete(campaignUpdateImage.getUrl());
-              campaignUpdateImageRepository.delete(campaignUpdateImage);
-            });
-  }
+    @Override
+    @Async
+    @Transactional
+    public void removeImages(CampaignUpdate campaignUpdate) {
+        campaignUpdateImageRepository
+                .findByCampaignUpdate(campaignUpdate)
+                .forEach(
+                        campaignUpdateImage -> {
+                            cloudObjectStorageService.delete(campaignUpdateImage.getUrl());
+                            campaignUpdateImageRepository.delete(campaignUpdateImage);
+                        });
+    }
 }
